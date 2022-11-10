b13238e9db6d9de3fb39532eafbe8e03f557547a
Gabi <gvinokur@gmail.com>
2022-11-10 11:26:45 -0600

    Adding group creation and listing

---
+++ pages/friend-groups/[group_id].tsx
@@ -0,0 +1,68 @@
+import {
+  createRecord,
+  getCurrentUserId,
+  initThinBackend,
+  ProdeGroup,
+  ProdeGroupParticipant,
+  query,
+  User
+} from "thin-backend";
+import {Box, Table, TableCell, TableHead, TableRow, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+
+
+type ProdeGroupPageProps = {
+  group: ProdeGroup,
+  groupParticipants: ProdeGroupParticipant[]
+}
+
+initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
+
+const ProdeGroupPage = ({ group, groupParticipants}: ProdeGroupPageProps) => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = await query('users').whereIn('id', [group.ownerUserId, ...groupParticipants.map(participant => participant.userId)]).fetch();
+      setUsers(users);
+    };
+
+    getUsers();
+  }, [group, groupParticipants])
+  return (
+    <Box>
+      <Typography variant={'h1'}>{group.name}</Typography>
+      <Table>
+        <TableHead>
+          <TableCell>P</TableCell>
+          <TableCell>Nombre</TableCell>
+          <TableCell>Puntos Totales</TableCell>
+          <TableCell>Puntos Fase de Grupos</TableCell>
+          <TableCell>Puntos Playoffs</TableCell>
+        </TableHead>
+        {users.map((user, index) => (
+          <TableRow key={user.id} selected={user.id === getCurrentUserId()}>
+            <TableCell>{index+1}</TableCell>
+            <TableCell>{user.nickname || user.email}</TableCell>
+            <TableCell>0</TableCell>
+            <TableCell>0</TableCell>
+            <TableCell>0</TableCell>
+          </TableRow>
+        ))}
+      </Table>
+    </Box>
+  )
+}
+
+export const getServerSideProps = async ({params}: {params: { group_id: string} }) => {
+  const currentUserId = getCurrentUserId();
+  const group: ProdeGroup = await query('prode_groups').where('id', params.group_id).fetchOne();
+  const groupParticipants: ProdeGroupParticipant[] = await query('prode_group_participants').where('prodeGroupId', params.group_id).fetch();
+  return {
+    props: {
+      group,
+      groupParticipants
+    }
+  }
+}
+
+export default ProdeGroupPage
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,137 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { query, initThinBackend, ensureIsUser, getCurrentUser } from 'thin-backend';
-import { useQuery, useCurrentUser } from 'thin-backend-react';
-import { useEffect, useState } from 'react';
-import styles from '../styles/Home.module.css'
-import {Box} from "@mui/material";
+import {
+  query,
+  initThinBackend,
+  createRecord,
+  getCurrentUserId,
+  ProdeGroup
+} from 'thin-backend';
+import {ChangeEvent, useEffect, useState} from 'react';
+import {
+  Button,
+  Card,
+  CardActions, CardContent,
+  CardHeader,
+  Dialog, DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid, IconButton, List, ListItem, ListItemText, TextField, Link
+} from "@mui/material";
+import {LoadingButton} from "@mui/lab";
+import { Share as ShareIcon } from "@mui/icons-material";
 
 
 initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
 
 const Home: NextPage = () => {
+  const [open, setOpen] = useState(false);
+  const [openSharingDialog, setOpenSharingDialog] = useState<string | false>(false);
+  const [groupName, setGroupName] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [userGroups, setUserGroups] = useState<ProdeGroup[]>([])
+  const [participantGroups, setParticipantGroups] = useState<ProdeGroup[]>([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const userGroups = await query('prode_groups').where('ownerUserId', getCurrentUserId()).fetch();
+      const groupParticipantIn = await query('prode_group_participants').where('userId', getCurrentUserId()).fetch();
+      const participantGroups = await query('prode_groups').whereIn('id', groupParticipantIn.map(groupParticipant => groupParticipant.prodeGroupId)).fetch();
+      setUserGroups(userGroups);
+      setParticipantGroups(participantGroups);
+    }
+
+    getData()
+  }, [getCurrentUserId()])
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleGroupNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setGroupName(e.target.value)
+  }
+  const handleGroupCreate = async () => {
+    setLoading(true)
+    await createRecord('prode_groups', { ownerUserId: getCurrentUserId(), name: groupName });
+    setLoading(false)
+    handleClose()
+  }
+
   return (
-    <Box>
-      {}
-    </Box>
+    <>
+      <Grid container>
+        <Grid item>
+          <Card>
+            <CardHeader title='Grupos de Amigos'/>
+            <CardContent>
+              <List>
+                {userGroups.map(userGroup => (
+                  <ListItem key={userGroup.id}
+                    secondaryAction={
+                      <IconButton onClick={() => setOpenSharingDialog(userGroup.id)}>
+                        <ShareIcon/>
+                      </IconButton>
+                    }>
+                    <ListItemText>{userGroup.name}</ListItemText>
+                  </ListItem>
+                ))}
+                <ListItem divider/>
+                {participantGroups.map(participantGroup => (
+                  <ListItem key={participantGroup.id}>
+                    <ListItemText>
+                      {participantGroup.name}
+                    </ListItemText>
+                  </ListItem>
+                ))}
+              </List>
+            </CardContent>
+            <CardActions>
+              <Button onClick={handleClickOpen}>Crear Nuevo Grupo</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Crear Grupo de Amigos</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Un grupo de amigos te permite tener un ranking privado de aciertos.
+            Crea tantos grupos de amigos como quieras, tu mismo pronostico va a ser usado para calcular tu posicion en todos.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Nombre"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={handleGroupNameChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button disabled={loading} onClick={handleClose}>Cancelar</Button>
+          <LoadingButton loading={loading} onClick={handleGroupCreate}>Crear</LoadingButton>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={!!openSharingDialog} onClose={() => setOpenSharingDialog(false)}>
+        <DialogTitle>Compartir Link</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Copia el siguiente link y compartilo con tus amigos para que se unan a tu grupo
+            &nbsp;<Link onClick={() => navigator.clipboard.writeText(`${window.location.origin}/friend-groups/join/${openSharingDialog}`)}>{`${window.location.origin}/friend-groups/join/${openSharingDialog}`}</Link>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button disabled={loading} onClick={handleClose}>Cancelar</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
