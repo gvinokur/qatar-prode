d195f9b5e6bb2a7b1f49f9f30452937333e13f3b
Gabi <gvinokur@gmail.com>
2024-06-23 16:33:30 -0500

    removing a lot of logs

--- app/actions/backoffice-actions.ts
+++ app/actions/backoffice-actions.ts
@@ -405,21 +405,15 @@ export async function recalculateAllPlayoffFirstRoundGameGuesses(tournamentId: s
 export async function calculateGameScores(forceDrafts: boolean, forceAllGuesses: boolean) {
   const gamesWithResultAndGuesses = await findAllGamesWithPublishedResultsAndGameGuesses(forceDrafts, forceAllGuesses)
   const gameGuessesToClean = await findAllGuessesForGamesWithResultsInDraft()
-  console.log('New games to update:', gamesWithResultAndGuesses.length )
   const updatedGameGuesses = await Promise.all(gamesWithResultAndGuesses.map(game => {
-    console.log('updating all score for game', game.game_number, 'with result', game.gameResult)
     const gameGuesses = game.gameGuesses
     return Promise.all(gameGuesses.map(gameGuess => {
       const score = calculateScoreForGame(game, gameGuess)
-      console.log('updating score for game guess on game', game.game_number, 'for user', gameGuess.user_id,
-        'with guess', gameGuess, 'to score', score)
       return updateGameGuess(gameGuess.id, {
         score
       })
     }))
   }))
-  console.log('Updated all scores for games')
-  console.log('Cleaning scores for games', gameGuessesToClean.length)
   const cleanedGameGuesses = await Promise.all(gameGuessesToClean.map(async (gameGuess) => {
     return updateGameGuess(gameGuess.id, {
       // @ts-ignore - setting to null to remove value
