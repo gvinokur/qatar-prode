505d1409dfa290bcf205d25a8823f5dae4c4a3ee
Gabi <gvinokur@gmail.com>
2026-04-01 11:36:08 -0500

    fix(analytics): add gtag initialization shim to layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -114,11 +114,24 @@ export default async function LocaleLayout({
           />
         )}
         {gaMeasurementId && !user?.isAdFree && (
-          <Script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
-            strategy="afterInteractive"
-          />
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaMeasurementId}', {
+                  send_page_view: false
+                });
+              `}
+            </Script>
+          </>
         )}
       </head>
       <body style={{minHeight: '100%'}}>
